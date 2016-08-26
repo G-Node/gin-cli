6f2b9aab3c35ec8eb2f5f0bb1d6db625fef0bb2c
Achilleas Koutsou <achilleas.k@gmail.com>
2016-08-26 13:41:56 +0200

    Handle entirety of login code in single function
    And store token

--- gin.go
+++ gin.go
@@ -66,12 +66,51 @@ func SearchAccount(query string) ([]proto.Account, error) {
 	return results, nil
 }
 
-func login(user string, pass string) (proto.TokenResponse, error) {
+func login(args map[string]interface{}) error {
+
+	var username, password string
+
+	if args["<username>"] == nil {
+		username = ""
+	} else {
+		username = args["<username>"].(string)
+	}
+
+	if username == "" {
+		// prompt for login
+		fmt.Print("Login: ")
+		username = ""
+		fmt.Scanln(&username)
+	}
+
+	// prompt for password
+	password = ""
+	fmt.Print("Password: ")
+	pwbytes, err := gopass.GetPasswdMasked()
+	fmt.Println()
+	if err != nil {
+		// read error or gopass.ErrInterrupted
+		if err == gopass.ErrInterrupted {
+			fmt.Println("Cancelled.")
+			return err
+		}
+		if err == gopass.ErrMaxLengthExceeded {
+			fmt.Println("Error: Input too long.")
+			return err
+		}
+	}
+
+	password = string(pwbytes)
+
+	if password == "" {
+		fmt.Println("No password provided. Aborting.")
+		return err
+	}
 
 	params := url.Values{}
 	params.Add("scope", "repo-read repo-write account-read account-write")
-	params.Add("username", user)
-	params.Add("password", pass)
+	params.Add("username", username)
+	params.Add("password", password)
 	params.Add("grant_type", "password")
 	params.Add("client_id", "gin")
 	params.Add("client_secret", "secret")
@@ -85,9 +124,9 @@ func login(user string, pass string) (proto.TokenResponse, error) {
 	var authresp proto.TokenResponse
 
 	if err != nil {
-		return authresp, err
+		return err
 	} else if res.StatusCode != 200 {
-		return authresp, fmt.Errorf("[Login error] %s", res.Status)
+		return fmt.Errorf("[Login error] %s", res.Status)
 	}
 
 	defer close(res.Body)
@@ -95,16 +134,24 @@ func login(user string, pass string) (proto.TokenResponse, error) {
 	b, err := ioutil.ReadAll(res.Body)
 
 	if err != nil {
-		return authresp, err
+		return err
 	}
 	err = json.Unmarshal(b, &authresp)
 	if err != nil {
-		return authresp, err
+		return err
 	}
 
 	// store token
+	err = ioutil.WriteFile("token", []byte(authresp.AccessToken), 0600)
 
-	return authresp, nil
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("[Login success] You are now logged in as %s\n", username)
+	fmt.Printf("You have been granted the following permissions: %v\n", strings.Replace(authresp.Scope, " ", ", ", -1))
+
+	return nil
 
 }
 
@@ -135,71 +182,36 @@ func GetSSHKeys(user string, token string) []proto.SSHKey {
 	return keys
 }
 
+func printAccountInfo(args map[string]interface{}) error {
+
+}
+
 func main() {
 	usage := `
 GIN command line client
 
 Usage:
 	gin login [<username>]
+	gin accountinfo [<username>]
 
 `
 
 	args, _ := docopt.Parse(usage, nil, true, "gin cli 0.0", false)
 
-	var username, password string
-
-	if args["<username>"] == nil {
-		username = ""
-	} else {
-		username = args["<username>"].(string)
-	}
-
-	if username == "" {
-		// prompt for login
-		fmt.Print("Login: ")
-		username = ""
-		fmt.Scanln(&username)
-	}
-
-	// prompt for password
-	password = ""
-	fmt.Print("Password: ")
-	pwbytes, err := gopass.GetPasswdMasked()
-	fmt.Println()
-	if err != nil {
-		// read error or gopass.ErrInterrupted
-		if err == gopass.ErrInterrupted {
-			fmt.Println("Cancelled.")
-			return
+	if args["login"].(bool) {
+		err := login(args)
+		if err != nil {
+			fmt.Println("Authentication failed!")
 		}
-		if err == gopass.ErrMaxLengthExceeded {
-			fmt.Println("Error: Input too long.")
-			return
-		}
+	} else if args["accountinfo"].(bool) {
+		err := printAccountInfo(args)
 	}
 
-	password = string(pwbytes)
+	// keys := GetSSHKeys()
+	// fmt.Printf("\nKey fingerprints:\n")
 
-	if password == "" {
-		fmt.Println("No password provided. Aborting.")
-		return
-	}
-
-	auth, err := login(username, password)
-	if err != nil {
-		fmt.Println("Authentication failed!")
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Printf("[Login success] You are now logged in as %s\n", username)
-	fmt.Printf("You have been granted the following permissions: %v\n", strings.Replace(auth.Scope, " ", ", ", -1))
-
-	keys := GetSSHKeys(username, auth.AccessToken)
-	fmt.Printf("\nKey fingerprints:\n")
-
-	for _, k := range keys {
-		fmt.Printf("\t• %s\n", k.Fingerprint)
-	}
+	// for _, k := range keys {
+	// 	fmt.Printf("\t• %s\n", k.Fingerprint)
+	// }
 
 }
