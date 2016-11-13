abfc7ee16745503ac9d2829e4ecb99473cee7c89
Achilleas Koutsou <achilleas.k@gmail.com>
2016-11-13 20:46:18 +0100

    [util] Error checking and termination functions
    Custom message function overrides error message.

--- util/die.go
+++ util/die.go
@@ -5,11 +5,27 @@ import (
 	"os"
 )
 
+//Die prints a message to stderr and exits the program.
+func Die(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+	os.Exit(1)
+}
+
 // CheckError exits the program if an error is passed to the function.
+// Before exiting, the error message is printed to stderr.
 // The function should be used to avoid constnatly checking if `err != nil` and returning errors up the stack when all that needs to be done is to stop execution.
 func CheckError(err error) {
 	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+		// TODO: Print error to log
+		Die(err.Error())
+	}
+}
+
+// CheckErrorMsg exits the program if an error is passed to the function.
+// Before exiting, the given msg string is printed to stderr.
+func CheckErrorMsg(err error, msg string) {
+	if err != nil {
+		// TODO: Print actual error to log
+		Die(msg)
 	}
 }
