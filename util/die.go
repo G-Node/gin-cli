a6762c0a2e54cdc63ff758041aa34bbeafa147cd
Achilleas Koutsou <achilleas.k@gmail.com>
2016-11-15 17:41:03 +0100

    [util] Typo fix

--- util/die.go
+++ util/die.go
@@ -13,7 +13,8 @@ func Die(msg string) {
 
 // CheckError exits the program if an error is passed to the function.
 // Before exiting, the error message is printed to stderr.
-// The function should be used to avoid constnatly checking if `err != nil` and returning errors up the stack when all that needs to be done is to stop execution.
+// The function should be used to avoid constantly checking if `err != nil` and
+// returning errors up the stack when all that needs to be done is to stop execution.
 func CheckError(err error) {
 	if err != nil {
 		// TODO: Print error to log
