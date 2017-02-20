64eabb86bb950849d3a1dcbab2dc573614bf5b54
Achilleas Koutsou <achilleas.k@gmail.com>
2017-02-20 19:22:30 +0100

    [util/xdg] Refactor out unnecessary makeDir func

--- util/xdg.go
+++ util/xdg.go
@@ -10,14 +10,6 @@ import (
 
 var suffix = "gin"
 
-func makePath(path string) error {
-	err := os.MkdirAll(path, 0777)
-	if err != nil {
-		return fmt.Errorf("Error accessing directory %s\n", path)
-	}
-	return nil
-}
-
 // ConfigPath returns the configuration path where configuration files should be stored.
 func ConfigPath(create bool) (string, error) {
 	// TODO: OS dependent paths
@@ -33,9 +25,9 @@ func ConfigPath(create bool) (string, error) {
 		path = filepath.Join(homedir, ".config", suffix)
 	}
 	if create {
-		err = makePath(path)
+		err = os.MkdirAll(path, 0777)
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("Error accessing directory %s\n", path)
 		}
 	}
 	return path, err
