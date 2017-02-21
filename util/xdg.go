6fd093775401053a020d8a1f1421c05ca34d38a4
Achilleas Koutsou <achilleas.k@gmail.com>
2017-02-21 16:25:06 +0100

    [util/xdg] Use usr.HomeDir instead of env $HOME

--- util/xdg.go
+++ util/xdg.go
@@ -5,6 +5,7 @@ package util
 import (
 	"fmt"
 	"os"
+	"os/user"
 	"path/filepath"
 )
 
@@ -12,22 +13,26 @@ var suffix = "gin"
 
 // ConfigPath returns the configuration path where configuration files should be stored.
 func ConfigPath(create bool) (string, error) {
-	// TODO: OS dependent paths
-	xdghome := os.Getenv("XDG_CONFIG_HOME")
-	homedir := os.Getenv("HOME")
-
+	// TODO: Handle Windows paths
 	var path string
 	var err error
 
-	if xdghome != "" {
-		path = filepath.Join(xdghome, suffix)
+	xdgconfig := os.Getenv("XDG_CONFIG_HOME")
+	if xdgconfig != "" {
+		path = filepath.Join(xdgconfig, suffix)
 	} else {
+		usr, err := user.Current()
+		if err != nil {
+			return "", fmt.Errorf("Error getting user home dir")
+		}
+		homedir := usr.HomeDir
 		path = filepath.Join(homedir, ".config", suffix)
 	}
+
 	if create {
 		err = os.MkdirAll(path, 0777)
 		if err != nil {
-			return "", fmt.Errorf("Error accessing directory %s\n", path)
+			return "", fmt.Errorf("Error creating directory %s", path)
 		}
 	}
 	return path, err
