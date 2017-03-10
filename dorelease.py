1a7b6071143a0c4f9917e11c98bdbc5507a8453d
Achilleas Koutsou <achilleas.k@gmail.com>
2017-03-10 19:38:32 +0100

    [dorelease] Python build and release script
    Builds the Linux binary, downloads the Windows binary from appveyor, and
    downloads git annex standalone for Linux

---
+++ dorelease.py
@@ -0,0 +1,145 @@
+import pickle
+import sys
+import os
+import json
+import requests
+import re
+from subprocess import check_output, call
+
+etagfile = "downloads/etags"
+etags = {}
+
+
+def load_etags():
+    try:
+        with open(etagfile, "rb") as fd:
+            etags.update(pickle.load(fd))
+    except FileNotFoundError:
+        print("--> No etags file found. Skipping load.")
+
+
+def save_etags():
+    with open(etagfile, "wb") as fd:
+        pickle.dump(etags, fd)
+
+
+def download(url, fname=None):
+    if fname is None:
+        fname = url.split("/")[-1]
+    fname = os.path.join("downloads", fname)
+    print("--> Downloading {} → {}".format(url, fname))
+    req = requests.get(url, stream=True)
+    size = req.headers.get("content-length")
+    et = req.headers.get("etag")
+    oldet = etags.get(url)
+    if et == oldet and os.path.exists(fname):
+        fod_size = os.path.getsize(fname)
+        if int(fod_size) == int(size):
+            print("File already downloaded. Skipping.")
+            return fname
+    etags[url] = et
+    prog = 0
+    with open(fname, "wb") as fd:
+        for chunk in req.iter_content(chunk_size=256):
+            fd.write(chunk)
+            prog += len(chunk)
+            print("\r{}/{}".format(prog, size), end="", flush=True)
+        print("\nDone!")
+    return fname
+
+
+def die(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+
+def wait_for_ret():
+    try:
+        input("Hit return to continue or ^C to cancel ...")
+    except KeyboardInterrupt:
+        die("\nCancelled")
+
+
+def build_linux():
+    print("--> Building Linux binary")
+    verfile = "version"
+    with open(verfile) as fd:
+        verinfo = fd.read()
+
+    verdict = {}
+    verdict["version"] = re.search(r"version=([v0-9\.]+)", verinfo).group(1)
+    verdict["build"] = re.search(r"build=([0-9]+)", verinfo).group(1)
+    cmd = ["git", "rev-parse", "HEAD"]
+    verdict["commit"] = check_output(cmd).strip().decode()
+    print(("Version: {version} "
+           "Build: {build} "
+           "Commit: {commit}").format(**verdict))
+    ldflags = ("-X main.version={version} "
+               "-X main.build={build} "
+               "-X main.commit={commit}").format(**verdict)
+    cmd = ["go", "build", "-ldflags", ldflags, "-o", "gin"]
+    ret = call(cmd)
+    if ret > 0:
+        die("Build failed")
+
+    print("--> Build succeeded")
+    cmd = ["./gin", "--version"]
+    verstring = check_output(cmd).strip().decode()
+    print("{}\n↪ {}".format(" ".join(cmd), verstring))
+
+    print("--> Updating version file")
+    verdict["build"] = "{:05d}".format(int(verdict["build"]) + 1)
+    newinfo = "version={version}\nbuild={build}\n".format(**verdict)
+    print(newinfo)
+    with open(verfile, "w") as fd:
+        fd.write(newinfo)
+
+
+def download_annex_sa():
+    annex_sa_url = ("https://downloads.kitenet.net/git-annex/linux/current/"
+                    "git-annex-standalone-amd64.tar.gz")
+    return download(annex_sa_url)
+
+
+def get_appveyor_artifact_url():
+    """
+    Queries Appveyor for the latest job artifacts. Returns the URL for the
+    latest 32bit binary only.
+    """
+    apiurl = "https://ci.appveyor.com/api/"
+    account = "achilleas-k"
+    project_name = "gin-cli"
+
+    url = os.path.join(apiurl, "projects", account, project_name)
+    r = requests.get(url)
+
+    projects = json.loads(r.text)
+    build = projects["build"]
+    for job in build["jobs"]:
+        if job["status"] == "success":
+            artifacts_url = os.path.join(apiurl, "buildjobs", job["jobId"],
+                                         "artifacts")
+            r = requests.get(artifacts_url)
+            artifacts = json.loads(r.text)
+            if "ARCH=32" in job["name"]:
+                a = artifacts[0]
+                arturl = os.path.join(apiurl, "buildjobs", job["jobId"],
+                                      "artifacts", a["fileName"])
+                return arturl
+
+
+def main():
+    try:
+        os.mkdir("downloads")
+    except FileExistsError:
+        pass
+    load_etags()
+    linux_file = build_linux()
+    win_url = get_appveyor_artifact_url()
+    win_file = download(win_url, "gin.exe")
+    annexsa_file = download_annex_sa()
+    save_etags()
+
+
+if __name__ == "__main__":
+    main()
