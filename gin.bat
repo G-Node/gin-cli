fad82830e7890007ca39d12b069372257105c883
Achilleas Koutsou <achilleas.k@gmail.com>
2017-03-16 16:07:31 +0100

    [gin.bat] A simpler batch file

--- gin.bat
+++ gin.bat
@@ -7,31 +7,19 @@
 ::
 :: The following actions are taken:
 ::
-:: - The gin configuration file (config.yml) is created and the binary paths
-:: for git, git-annex, and ssh are set.
-:: - The configuration file is copied to its intended location.
-:: - The location of gin.exe is appended to the PATH variable to it can be
+:: - The location of gin.exe is appended to the PATH variable so it can be
 :: used from the shell.
+:: - The locations of the git, annex, and utility binaries (SSH, rsync, etc)
+:: are appended to the PATH variable.
 :: - A shell is started in the user's home directory.
 
 echo off
-set gitcmd=%CD%\git\bin\git.exe
-set sshcmd=%CD%\git\usr\bin\ssh.exe
-set gitannexcmd=%CD%\git\usr\bin\git-annex.exe
 
-set configloc=%USERPROFILE%\.config\gin\
-set config=config.yml
+set ginbinpath=%CD%\bin
+set gitpaths=%CD%\git\usr\bin;%CD%\git\bin
 
-echo bin: > %config%
-echo     git: %gitcmd% >> %config% 
-echo     ssh: %sshcmd% >> %config% 
-echo     gitannex: %gitannexcmd% >> %config%
+path %path%;%ginbinpath%;%gitpaths%
 
-setlocal enableextensions
-md %configloc%
-move config.yml %configloc%
-
-path %path%;%CD%\bin;%CD%\git\usr\bin;%CD%\git\bin\
 cd %USERPROFILE%
 
 cmd /k
