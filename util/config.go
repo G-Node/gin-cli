6247a9fee623a0ff8a2106dbf5bd07d36d4f993b
Achilleas Koutsou <achilleas.k@gmail.com>
2017-05-15 16:19:00 +0200

    [util] Client secret can be configured
    New config option: "secret"
    Sets the client secret and defaults to the gin.g-node server value (the
    previous built-in).
    
    Closes #75

--- util/config.go
+++ util/config.go
@@ -22,6 +22,7 @@ type conf struct {
 		Exclude []string
 		MinSize string
 	}
+	Secret string
 }
 
 // Config makes the configuration options available after LoadConfig is called
@@ -42,6 +43,7 @@ func LoadConfig() error {
 	viper.SetDefault("git.address", "gin.g-node.org")
 	viper.SetDefault("git.port", "22")
 	viper.SetDefault("git.user", "git")
+	viper.SetDefault("secret", "97196a1c-silly-biscuit3-d161ea15a676")
 
 	// annex filters
 	viper.SetDefault("annex.exclude", [...]string{"*.md", "*.rst", "*.txt", "*.c", "*.cpp", "*.h", "*.hpp", "*.py", "*.go"})
@@ -91,6 +93,9 @@ func LoadConfig() error {
 
 	LogWrite("Configuration values")
 	LogWrite("%+v", Config)
+
+	// Set secret after printing configuration to log file
+	Config.Secret = viper.GetString("secret")
 
 	return nil
 }
