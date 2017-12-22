a8efa635518ea3bd521833100e45c0bd3499774e
Achilleas Koutsou <achilleas.k@gmail.com>
2017-12-22 18:50:59 +0100

    [web] Error type for web client package
    This Error type implements the built-in error interface. In addition to
    storing an error message, it also holds the name of the originating
    function (from gin-cli/web) and the original, underlying error message
    (from a system or API call).

--- web/web.go
+++ web/web.go
@@ -16,6 +16,23 @@ import (
 	gogs "github.com/gogits/go-gogs-client"
 )
 
+// Error is used to return errors caused by web requests, API calls, or system calls in the web package
+type Error struct {
+	// The error that was returned by the underlying system or API call
+	UError string
+	// The function where the error originated
+	Origin string
+	// Human-readable description of error and conditions
+	Description string
+}
+
+func (e Error) Error() string {
+	if e.Description != "" {
+		return e.Description
+	}
+	return e.UError
+}
+
 // UserToken struct for username and token
 type UserToken struct {
 	Username string
@@ -46,7 +63,7 @@ func (cl *Client) Get(address string) (*http.Response, error) {
 	requrl := urlJoin(cl.Host, address)
 	req, err := http.NewRequest("GET", requrl, nil)
 	if err != nil {
-		return nil, err
+		return nil, Error{UError: err.Error(), Origin: fmt.Sprintf("Get(%s)", requrl)}
 	}
 	req.Header.Set("content-type", "application/jsonAuthorization")
 	util.LogWrite("Performing GET with token: %s", cl.Token)
@@ -55,20 +72,25 @@ func (cl *Client) Get(address string) (*http.Response, error) {
 		util.LogWrite("Added token to GET")
 	}
 	util.LogWrite("Performing GET: %s", req.URL)
-	return cl.web.Do(req)
+	resp, err := cl.web.Do(req)
+	if err != nil {
+		return nil, Error{UError: err.Error(), Origin: fmt.Sprintf("Get(%s)", requrl)}
+	}
+	return resp, err
 }
 
 // Post sends a POST request to address with the provided data.
 // The address is appended to the client host, so it should be specified without a host prefix.
 func (cl *Client) Post(address string, data interface{}) (*http.Response, error) {
+	fn := fmt.Sprintf("Post(%s, <data>)", address)
 	datajson, err := json.Marshal(data)
 	if err != nil {
-		return nil, err
+		return nil, Error{UError: err.Error(), Origin: fn}
 	}
 	requrl := urlJoin(cl.Host, address)
 	req, err := http.NewRequest("POST", requrl, bytes.NewReader(datajson))
 	if err != nil {
-		return nil, err
+		return nil, Error{UError: err.Error(), Origin: fn}
 	}
 	req.Header.Set("content-type", "application/jsonAuthorization")
 	if cl.Token != "" {
@@ -76,33 +98,43 @@ func (cl *Client) Post(address string, data interface{}) (*http.Response, error)
 		util.LogWrite("Added token to POST")
 	}
 	util.LogWrite("Performing POST: %s", req.URL)
-	return cl.web.Do(req)
+	resp, err := cl.web.Do(req)
+	if err != nil {
+		err = Error{UError: err.Error(), Origin: fn}
+	}
+	return resp, err
 }
 
 // PostBasicAuth sends a POST request to address with the provided data.
 // The username and password are used to perform Basic authentication.
 func (cl *Client) PostBasicAuth(address, username, password string, data interface{}) (*http.Response, error) {
+	fn := fmt.Sprintf("PostBasicAuth(%s)", address)
 	datajson, err := json.Marshal(data)
 	if err != nil {
-		return nil, err
+		return nil, Error{UError: err.Error(), Origin: fn}
 	}
 	requrl := urlJoin(cl.Host, address)
 	req, err := http.NewRequest("POST", requrl, bytes.NewReader(datajson))
 	if err != nil {
-		return nil, err
+		return nil, Error{UError: err.Error(), Origin: fn}
 	}
 	req.Header.Set("content-type", "application/json")
 	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", gogs.BasicAuthEncode(username, password)))
 	util.LogWrite("Performing POST: %s", req.URL)
-	return cl.web.Do(req)
+	resp, err := cl.web.Do(req)
+	if err != nil {
+		err = Error{UError: err.Error(), Origin: fn}
+	}
+	return resp, err
 }
 
 // Delete sends a DELETE request to address.
 func (cl *Client) Delete(address string) (*http.Response, error) {
+	fn := fmt.Sprintf("Delete(%s)", address)
 	requrl := urlJoin(cl.Host, address)
 	req, err := http.NewRequest("DELETE", requrl, nil)
 	if err != nil {
-		return nil, err
+		return nil, Error{UError: err.Error(), Origin: fn}
 	}
 	req.Header.Set("content-type", "application/jsonAuthorization")
 	if cl.Token != "" {
@@ -110,7 +142,11 @@ func (cl *Client) Delete(address string) (*http.Response, error) {
 		util.LogWrite("Added token to DELETE")
 	}
 	util.LogWrite("Performing DELETE: %s", req.URL)
-	return cl.web.Do(req)
+	resp, err := cl.web.Do(req)
+	if err != nil {
+		err = Error{UError: err.Error(), Origin: fn}
+	}
+	return resp, err
 }
 
 // NewClient creates a new client for a given host.
@@ -121,24 +157,22 @@ func NewClient(host string) *Client {
 // LoadToken reads the username and auth token from the token file and sets the
 // values in the struct.
 func (ut *UserToken) LoadToken() error {
+	fn := "LoadToken()"
 	if ut.Username != "" && ut.Token != "" {
 		return nil
 	}
-	path, err := util.ConfigPath(false)
-	if err != nil {
-		return fmt.Errorf("unable to access config directory")
-	}
+	path, _ := util.ConfigPath(false) // Error can only occur when create=True
 	filepath := filepath.Join(path, "token")
 	file, err := os.Open(filepath)
 	if err != nil {
-		return fmt.Errorf("failed to load user token")
+		return Error{UError: err.Error(), Origin: fn, Description: "failed to load user token"}
 	}
 	defer closeFile(file)
 
 	decoder := gob.NewDecoder(file)
 	err = decoder.Decode(ut)
 	if err != nil {
-		return err
+		return Error{UError: err.Error(), Origin: fn, Description: "failed to parse user token"}
 	}
 	util.LogWrite("Token read OK")
 	return nil
@@ -146,22 +180,25 @@ func (ut *UserToken) LoadToken() error {
 
 // StoreToken saves the username and auth token to the token file.
 func (ut *UserToken) StoreToken() error {
-	util.LogWrite("Saving token. ")
+	fn := "StoreToken()"
+	util.LogWrite("Saving token")
 	path, err := util.ConfigPath(true)
 	if err != nil {
-		return fmt.Errorf("could not creating or accessing config directory.")
+		return Error{UError: err.Error(), Origin: fn}
 	}
 	filepath := filepath.Join(path, "token")
 	file, err := os.Create(filepath)
 	if err != nil {
-		return fmt.Errorf("Error saving user token.")
+		util.LogWrite("Failed to create token file %s", filepath)
+		return Error{UError: err.Error(), Origin: fn, Description: fmt.Sprintf("failed to create token file %s", filepath)}
 	}
 	defer closeFile(file)
 
 	encoder := gob.NewEncoder(file)
 	err = encoder.Encode(ut)
 	if err != nil {
-		return err
+		util.LogWrite("Failed to write token to file %s", filepath)
+		return Error{UError: err.Error(), Origin: fn, Description: "failed to store token"}
 	}
 	util.LogWrite("Saved")
 	return nil
@@ -169,14 +206,11 @@ func (ut *UserToken) StoreToken() error {
 
 // DeleteToken deletes the token file if it exists (for finalising a logout).
 func DeleteToken() error {
-	path, err := util.ConfigPath(false)
-	if err != nil {
-		return fmt.Errorf("Could not delete token: Error accessing config directory.")
-	}
+	path, _ := util.ConfigPath(false) // Error can only occur when create=True
 	tokenpath := filepath.Join(path, "token")
-	err = os.Remove(tokenpath)
+	err := os.Remove(tokenpath)
 	if err != nil {
-		return err
+		return Error{UError: err.Error(), Origin: "DeleteToken()", Description: "could not delete token"}
 	}
 	util.LogWrite("Token deleted")
 	return nil
