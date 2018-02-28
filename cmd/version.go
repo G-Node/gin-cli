80aaf5885e2484e2faf46b78af04e17354e0712e
Achilleas Koutsou <achilleas.k@gmail.com>
2018-02-28 18:51:29 +0100

    Merge branch master into versioning
    [version] Versioning command from pre-merge
    [upload] commitmsg

---
+++ cmd/version.go
@@ -0,0 +1,92 @@
+package gincmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	ginclient "github.com/G-Node/gin-cli/gin-client"
+	"github.com/G-Node/gin-cli/util"
+	"github.com/spf13/cobra"
+)
+
+func repoversion(cmd *cobra.Command, args []string) {
+	if !ginclient.IsRepo() {
+		util.Die("This command must be run from inside a gin repository.")
+	}
+	count, _ := cmd.Flags().GetUint("max-count")
+	jsonout, _ := cmd.Flags().GetBool("json")
+	commits, err := ginclient.GitLog(count)
+	util.CheckError(err)
+	if jsonout {
+		j, _ := json.Marshal(commits)
+		fmt.Println(string(j))
+		return
+	}
+	commit := verprompt(commits)
+
+	gincl := ginclient.NewClient(util.Config.GinHost)
+	requirelogin(cmd, gincl, true) // TODO: change when we support offline-only
+	checkout(commit, gincl)
+}
+
+func verprompt(commits []ginclient.GinCommit) ginclient.GinCommit {
+	ndigits := len(strconv.Itoa(len(commits) + 1))
+	numfmt := fmt.Sprintf("[%%%dd]", ndigits)
+	for idx, commit := range commits {
+		idxstr := fmt.Sprintf(numfmt, idx+1)
+		fmt.Printf("%s  %s * %s\n\n", idxstr, green(commit.AbbreviatedHash), commit.Date.Format("Mon Jan 2 15:04:05 2006 (-0700)"))
+		fmt.Printf("\t%s\n\n", commit.Subject)
+		fstats := commit.FileStats
+		if len(fstats.NewFiles) > 0 {
+			fmt.Printf("\tAdded:    %s\n", strings.Join(fstats.NewFiles, ", "))
+		}
+		if len(fstats.ModifiedFiles) > 0 {
+			fmt.Printf("\tModified: %s\n", strings.Join(fstats.ModifiedFiles, ", "))
+		}
+		if len(fstats.DeletedFiles) > 0 {
+			fmt.Printf("\tDeleted:  %s\n", strings.Join(fstats.DeletedFiles, ", "))
+		}
+		fmt.Println()
+	}
+	var selstr string
+	fmt.Print("Version to change to: ")
+	fmt.Scanln(&selstr)
+
+	num, err := strconv.Atoi(selstr)
+	if err == nil && num > 0 && num <= len(commits) {
+		return commits[num-1]
+	}
+
+	// try to match hash
+	for _, commit := range commits {
+		if commit.AbbreviatedHash == selstr {
+			return commit
+		}
+	}
+
+	util.Die("Aborting")
+	return ginclient.GinCommit{}
+}
+
+func checkout(commit ginclient.GinCommit, gincl *ginclient.Client) {
+	if !ginclient.IsRepo() {
+		util.Die("This command must be run from inside a gin repository.")
+	}
+	ginclient.GitCheckout(commit.Hash, nil)
+	uploadchan := make(chan ginclient.RepoFileStatus) // TODO: change to commit when we support offline-only
+	hostname, err := os.Hostname()
+	if err != nil {
+		util.LogWrite("Could not retrieve hostname")
+		hostname = "(unknown)"
+	}
+
+	commitsubject := fmt.Sprintf("Repository version changed by %s@%s", gincl.Username, hostname)
+	commitbody := fmt.Sprintf("Returning to version as of %s\nVersion ID: %s\n%s", commit.Date.Format("Mon Jan 2 15:04:05 2006 (-0700)"), commit.AbbreviatedHash, getchanges())
+	commitmsg := fmt.Sprintf("%s\n\n%s", commitsubject, commitbody)
+
+	go gincl.Upload([]string{}, commitmsg, uploadchan)
+	printProgress(uploadchan, false)
+}
