691f9c5438a85513db0e60b1a64ad39d4e225c54
Achilleas Koutsou <achilleas.k@gmail.com>
2018-03-13 17:44:26 +0100

    [die.go] Don't print line if error message empty

--- util/die.go
+++ util/die.go
@@ -14,8 +14,13 @@ var red = color.New(color.FgRed).SprintFunc()
 func Die(msg interface{}) {
 	// fmt.Fprintf(color.Error, "%s %s\n", red("ERROR"), msg)
 	// Swap the line above for the line below when (if) https://github.com/fatih/color/pull/87 gets merged
-	fmt.Fprintln(os.Stderr, msg)
-	LogWrite("Exiting with ERROR message: %s", msg)
+	msgstring := fmt.Sprintf("%s", msg)
+	if len(msgstring) > 0 {
+		LogWrite("Exiting with ERROR message: %s", msg)
+		fmt.Fprintln(os.Stderr, msg)
+	} else {
+		LogWrite("Exiting with ERROR (no message)")
+	}
 	LogClose()
 	os.Exit(1)
 }
