a6db4935b1b7b08775fa913c702b7078081471fa
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-24 14:17:44 +0200

    [upload] Add flag for specifying remote for upload

--- gincmd/uploadcmd.go
+++ gincmd/uploadcmd.go
@@ -40,14 +40,22 @@ func upload(cmd *cobra.Command, args []string) {
 func UploadCmd() *cobra.Command {
 	description := "Upload changes made in a local repository clone to the remote repository on the GIN server. This command must be called from within the local repository clone. Specific files or directories may be specified. All changes made will be sent to the server, including addition of new files, modifications and renaming of existing files, and file deletions.\n\nIf no arguments are specified, only changes to files already being tracked are uploaded."
 	args := map[string]string{"<filenames>": "One or more directories or files to upload and update."}
+	examples := map[string]string{
+		"Upload 'data1.dat' and 'values.csv' to default remote":             "$ gin upload data1.dat values.csv",
+		"Upload all files in current directory to default remote":           "$ gin upload .",
+		"Upload all previously committed changes to remote named 'labdata'": "$ gin upload --to labdata",
+		"Upload all '.zip' files to remotes named 'gin' and 'labdata'":      "$ gin upload --to gin --to labdata *.zip\n    or\n$ gin upload --to gin,labdata *.zip",
+	}
 	var uploadCmd = &cobra.Command{
-		Use:   "upload [--json] [<filenames>]...",
-		Short: "Upload local changes to a remote repository",
-		Long:  formatdesc(description, args),
-		Args:  cobra.ArbitraryArgs,
-		Run:   upload,
+		Use:     "upload [--json] [--to <remote>] [<filenames>]...",
+		Short:   "Upload local changes to a remote repository",
+		Long:    formatdesc(description, args),
+		Args:    cobra.ArbitraryArgs,
+		Example: formatexamples(examples),
+		Run:     upload,
 		DisableFlagsInUseLine: true,
 	}
 	uploadCmd.Flags().Bool("json", false, "Print output in JSON format.")
+	uploadCmd.Flags().StringSliceP("to", "t", nil, "Upload to specific `remote`. Supports multiple remotes, either by specifying multiple times or as a comma separated list (see Examples).")
 	return uploadCmd
 }
