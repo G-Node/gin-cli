6c87cfc4d889b6e78071debb5129f2965db453eb
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-25 17:57:10 +0200

    [use-remote] Print current default if no argument

--- gincmd/useremote.go
+++ gincmd/useremote.go
@@ -13,23 +13,29 @@ func setRemote(cmd *cobra.Command, args []string) {
 	if !git.IsRepo() {
 		Die(ginerrors.NotInRepo)
 	}
-	name := args[0]
-	err := ginclient.SetDefaultRemote(name)
+	if len(args) > 0 {
+		name := args[0]
+		err := ginclient.SetDefaultRemote(name)
+		CheckError(err)
+	}
+	name, err := ginclient.DefaultRemote()
 	CheckError(err)
 	fmt.Printf(":: Default remote: %s\n", name)
 }
 
 // UseRemoteCmd sets up the 'use-remote' repository subcommand
 func UseRemoteCmd() *cobra.Command {
-	description := "Set the default remote for uploading. The default remote is used when 'gin upload' is invoked without specifying the --to option."
+	description := `Set the default remote for uploading. The default remote is used when 'gin upload' is invoked without specifying the --to option.
+	
+With no arguments, this command simply prints the currently configured default remote.`
 	args := map[string]string{
 		"<name>": "The name of the remote to use by default",
 	}
 	var addRemoteCmd = &cobra.Command{
-		Use:   "use-remote <name>",
+		Use:   "use-remote [<name>]",
 		Short: "Set the repository's default upload remote",
 		Long:  formatdesc(description, args),
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.MaximumNArgs(1),
 		Run:   setRemote,
 		DisableFlagsInUseLine: true,
 	}
