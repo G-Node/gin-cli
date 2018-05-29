0a2bec2a2aa9803f75dec59bd862a5c6e389ccf4
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-29 20:11:46 +0200

    [config] Support multiple server configurations
    Reads in a map of string->ServerCfg, where the key is the alias/name of
    the server.

--- ginclient/config/config.go
+++ ginclient/config/config.go
@@ -2,29 +2,47 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/G-Node/gin-cli/ginclient/log"
+	"github.com/fatih/color"
 	"github.com/shibukawa/configdir"
 	"github.com/spf13/viper"
 )
 
-var configDirs = configdir.New("g-node", "gin")
+var (
+	configDirs = configdir.New("g-node", "gin")
+	yellow     = color.New(color.FgYellow).SprintFunc()
+)
 
 // WebCfg is the configuration for the web server.
 type WebCfg struct {
 	Protocol string
 	Host     string
-	Port     string
+	Port     uint16
+}
+
+// AddressStr constructs a full address string from the configuration.
+// The string has the format Scheme://Host:Port (e.g., https://web.gin.g-node.org:443)
+func (c WebCfg) AddressStr() string {
+	return fmt.Sprintf("%s://%s:%d", c.Protocol, c.Host, c.Port)
 }
 
 // GitCfg is the configuration for the git server.
 type GitCfg struct {
-	User string
-	Host string
-	Port string
+	User    string
+	Host    string
+	Port    uint16
+	HostKey string
+}
+
+// AddressStr constructs a full address string from the configuration.
+// The string has the format ssh://User@Host:Port (e.g., ssh://git@gin.g-node.org:22)
+func (c GitCfg) AddressStr() string {
+	return fmt.Sprintf("ssh://%s@%s:%d", c.User, c.Host, c.Port)
 }
 
 // ServerCfg holds the information required for GIN servers (web and git).
@@ -35,11 +53,8 @@ type ServerCfg struct {
 
 // GinCliCfg holds the client configuration values.
 type GinCliCfg struct {
-	GinHost    string
-	GitHost    string
-	GitUser    string
-	GitHostKey string
-	Bin        struct {
+	Servers map[string]ServerCfg
+	Bin     struct {
 		Git      string
 		GitAnnex string
 		SSH      string
@@ -94,15 +109,6 @@ func Read() GinCliCfg {
 	viper.SetDefault("bin.gitannex", "git-annex")
 	viper.SetDefault("bin.ssh", "ssh")
 
-	// Hosts
-	viper.SetDefault("gin.address", "https://web.gin.g-node.org")
-	viper.SetDefault("gin.port", "443")
-
-	viper.SetDefault("git.address", "gin.g-node.org")
-	viper.SetDefault("git.port", "22")
-	viper.SetDefault("git.user", "git")
-	viper.SetDefault("git.hostkey", "gin.g-node.org,141.84.41.216 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBE5IBgKP3nUryEFaACwY4N3jlqDx8Qw1xAxU2Xpt5V0p9RNefNnedVmnIBV6lA3n+9kT1OSbyqA/+SgsQ57nHo0=")
-
 	// annex filters
 	viper.SetDefault("annex.minsize", "10M")
 
@@ -117,19 +123,43 @@ func Read() GinCliCfg {
 		log.Write("Found config file %s", confpath)
 	}
 
+	// Servers
+	servers := make(map[string]ServerCfg)
+
+	// append default gin configuration
+	servers["gin"] = ServerCfg{
+		WebCfg{
+			Protocol: "https",
+			Host:     "web.gin.g-node.org",
+			Port:     443,
+		},
+		GitCfg{
+			Host:    "gin.g-node.org",
+			Port:    22,
+			User:    "git",
+			HostKey: "gin.g-node.org,141.84.41.216 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBE5IBgKP3nUryEFaACwY4N3jlqDx8Qw1xAxU2Xpt5V0p9RNefNnedVmnIBV6lA3n+9kT1OSbyqA/+SgsQ57nHo0=",
+		},
+	}
+
+	// Read the config, overwriting the "gin" configuration if it exists
+	srvcfgMap := viper.GetStringMap("servers")
+	for alias, cfg := range srvcfgMap {
+		// Marshal map then unmarshal into ServerCfg struct
+		marshaled, _ := json.Marshal(cfg)
+		s := ServerCfg{}
+		err := json.Unmarshal(marshaled, &s)
+		if err != nil {
+			fmt.Fprintf(color.Output, "%s invalid value found in configuration for '%s': server configuration ignored\n", yellow("[warning]"), alias)
+			continue
+		}
+		servers[alias] = s
+	}
+	configuration.Servers = servers
+
+	// Binaries
 	configuration.Bin.Git = viper.GetString("bin.git")
 	configuration.Bin.GitAnnex = viper.GetString("bin.gitannex")
 	configuration.Bin.SSH = viper.GetString("bin.ssh")
-
-	ginAddress := viper.GetString("gin.address")
-	ginPort := viper.GetInt("gin.port")
-	configuration.GinHost = fmt.Sprintf("%s:%d", ginAddress, ginPort)
-
-	gitAddress := viper.GetString("git.address")
-	gitPort := viper.GetInt("git.port")
-	configuration.GitHost = fmt.Sprintf("%s:%d", gitAddress, gitPort)
-	configuration.GitUser = viper.GetString("git.user")
-	configuration.GitHostKey = viper.GetString("git.hostkey")
 
 	// configuration file in the repository root (annex excludes and size threshold only)
 	reporoot, err := findreporoot(".")
