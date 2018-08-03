c8bcf649241b2e43592d9f6fc8579ff209e64413
Achilleas Koutsou <achilleas.k@gmail.com>
2018-08-03 15:51:28 +0200

    Change min annex version to 6.20171108
    Discovered by testing available git-annex-standalone versions.
    Refactored version comparison function.

--- gincmd/cliversion.go
+++ gincmd/cliversion.go
@@ -7,7 +7,8 @@ import (
 	version "github.com/hashicorp/go-version"
 )
 
-const minAnnexVersion = "6.20160126" // Introduction of git-annex add --json
+const minAnnexVersion = "6.20171108" // tested working
+var minver, _ = version.NewVersion(minAnnexVersion)
 
 // VersionInfo holds the version numbers supplied by the linker flags in a convenient struct.
 type VersionInfo struct {
@@ -55,21 +56,27 @@ func (v *VersionInfo) GitOK() (bool, error) {
 // AnnexOK checks if the system annex version is higher than the required one.
 // If it is not, or the git-annex binary is not found, an appropriate error message is returned.
 func (v *VersionInfo) AnnexOK() (bool, error) {
-	systemver, err := version.NewVersion(v.Annex)
-	if err != nil {
-		// Special case for neurodebian git-annex version
-		// The version string contains a tilde as a separator for the arch suffix
-		// Cutting off the suffix and checking again
-		verstring := strings.Split(v.Annex, "~")[0]
-		systemver, err = version.NewVersion(verstring)
-		if err != nil {
-			// Can't figure out the version: print error from AnnexVersion
-			return false, fmt.Errorf(v.Annex)
+	compare := func(sv *version.Version) (bool, error) {
+		if sv.LessThan(minver) {
+			return false, fmt.Errorf("git-annex version %s found, but %s or newer is required", v.Annex, minAnnexVersion)
 		}
+		return true, nil
 	}
-	minver, _ := version.NewVersion(minAnnexVersion)
-	if systemver.LessThan(minver) {
-		return false, fmt.Errorf("git-annex version %s found, but %s or newer is required", v.Annex, minAnnexVersion)
+
+	systemver, err := version.NewVersion(v.Annex)
+	if err == nil {
+		return compare(systemver)
 	}
-	return true, nil
+
+	// Special case for neurodebian git-annex version
+	// The version string contains a tilde as a separator for the arch suffix
+	// Cutting off the suffix and checking again
+	verstring := strings.Split(v.Annex, "~")[0]
+	systemver, err = version.NewVersion(verstring)
+	if err == nil {
+		return compare(systemver)
+	}
+
+	// Can't figure out the version: print error from AnnexVersion
+	return false, fmt.Errorf(v.Annex)
 }
