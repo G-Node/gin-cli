480ec8c688b26518c8bc82f4b1f6265aa0e05719
Achilleas Koutsou <achilleas.k@gmail.com>
2019-03-05 20:56:36 +0100

    [unlock] Typo fix in help text

--- gincmd/unlockcmd.go
+++ gincmd/unlockcmd.go
@@ -48,7 +48,7 @@ func unlock(cmd *cobra.Command, args []string) {
 func UnlockCmd() *cobra.Command {
 	description := "Unlock one or more files for editing. Files added to the repository are left in a locked state, which allows reading but prevents editing. In order to edit or write to a file, it must first be unlocked. When done editing, it is recommended that the file be locked again using the 'lock' command.\n\nAfter performing an 'upload, 'download', or 'get', affected files are always reverted to the locked state.\n\nUnlocking a file takes longer depending on its size."
 	args := map[string]string{
-		"<filenames>": "One or more directories or files to unllock.",
+		"<filenames>": "One or more directories or files to unlock.",
 	}
 	var cmd = &cobra.Command{
 		Use:                   "unlock [--json | --verbose] <filenames>...",
